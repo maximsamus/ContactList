03fff52902dd6679536e624a6654fa7c643182af
maximsamus <maximsamus1@gmail.com>
2022-05-26 13:58:34 +0300

    Data model

---
+++ ContactList/Person.swift
@@ -0,0 +1,58 @@
+//
+//  Person.swift
+//  HW3
+//
+//  Created by Alexey Efimov on 17.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
